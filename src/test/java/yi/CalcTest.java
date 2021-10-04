0d1411d5b65b45728035b84488061b9808a0ed15
jiaodongyi <65889912+jiaodongyi@users.noreply.github.com>
2021-10-04 19:21:24 +0800

    Add files via upload

---
+++ src/test/java/yi/CalcTest.java
@@ -0,0 +1,26 @@
+package yi;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+    /**
+     * Create the test case
+     *
+     */
+    @Test
+    public void appTest()
+    {
+
+        assertEquals(Calc.add(1,1), 2);
+    }
+    
+
+}
